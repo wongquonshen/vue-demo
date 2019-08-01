c51f672e950e518dfb7d112e727a06a631ca1a91
wongquonshen <quonshenwong@outlook.com>
2019-08-01 22:55:41 +0800

    new vue project

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <Row>
+        
+    </Row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import iView from "iview"
+import locale from "iview/dist/locale/en-US"
 
+import "iview/dist/styles/iview.css"
+
+Vue.use(iView, { locale })
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
